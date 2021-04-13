f7dd6ca96bfd4889f7aeb4b62ccd0230daf72dba
arzoouzma01 <arzoouzma01@gmail.com>
2021-04-12 20:41:23 -0400

    Updates

---
+++ precache-manifest.9e12f4d03359419e04aae6b6d60fc46e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0fad4c0f10d57b530fe4",
+    "url": "/portfolio/static/js/runtime~main.0fad4c0f.js"
+  },
+  {
+    "revision": "1970515300e643332d01",
+    "url": "/portfolio/static/js/main.19705153.chunk.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/portfolio/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "1970515300e643332d01",
+    "url": "/portfolio/static/css/main.9e8527c4.chunk.css"
+  },
+  {
+    "revision": "79fa242f9a80d2cd0a0b90d6ad4b19d0",
+    "url": "/portfolio/index.html"
+  }
+];
